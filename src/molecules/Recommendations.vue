a86483b151c5fa8fc8dc163ed4d551c9f175f505
Sam Feder <sam.feder1@gmail.com>
2021-04-03 21:33:15 -0400

    setting up recomendations

--- src/molecules/Recommendations.vue
+++ src/molecules/Recommendations.vue
@@ -1,26 +1,36 @@
 <template>
   <div>
-    <h1>This is the Recommendations Section</h1>
-    <p>We have lots of recommendations</p>
-    <p>We have lots of recommendations</p>
-    <p>We have lots of recommendations</p>
-    <p>We have lots of recommendations</p>
-    <p>We have lots of recommendations</p>
-        <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+    <p class="guestbook-header">We hope you enjoy your stay at the Sunrise Sanctuary.</p>
+    <p class="guestbook-descriptor">
+      We thought we would share with you our absolute favorite places to eat, enjoy a cup of coffee, do a fun activity or explore!
+    </p>
+    <div class="location-guide-title-restaurants">
+      <h2 class="guide-header">Best Restaurants and Bars</h2>
+    </div>
   </div>
 </template>
 
-<script>
+<script> 
 export default {
 
 }
 </script>
 
 <style>
+.location-guide-title-restaurants {
+    padding-top: 80px;
+    padding-bottom: 80px;
+    background-image: url('~@/assets/resto.jpeg');
+    background-position: 50% 50%;
+    background-size: cover;
+}
 
+.guide-header {
+    margin-left: 0px;
+    color: #fff;
+    font-size: 36px;
+    line-height: 48px;
+    letter-spacing: -2px;
+    text-shadow: 1px 1px 4px rgb(0 0 0 / 10%);
+}
 </style>
